0dc9bafd2df47351db4cc4891e607618227675a3
Emir <emirgungor07@hotmail.com>
2024-10-31 11:40:30 +0300

    Added backend functions

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -2,17 +2,30 @@
 import React, { useState } from "react";
 import "../css/currency.css";
 import { FaLongArrowAltRight } from "react-icons/fa";
+import { FaExchangeAlt } from "react-icons/fa";
+
+import axios from "axios";
+
+let BASE_URL = "https://api.freecurrencyapi.com/v1/latest";
+let API_KEY = "fca_live_eG1QfuVZDcMeBxhjgW7BaqPU3ieBFBAuhtWPASII";
 
 const Currency = () => {
-  const [amount, setAmount] = useState();
-  const [fromCurrency, setFromCurrency] = useState("");
-  const [toCurrency, setToCurrency] = useState("");
-  const [result, setResult] = useState();
+  const [amount, setAmount] = useState(0);
+  const [fromCurrency, setFromCurrency] = useState("PLN");
+  const [toCurrency, setToCurrency] = useState("TRY");
+  const [result, setResult] = useState(0);
 
-  const exchange = () => {
-    console.log(amount);
-    console.log(fromCurrency);
-    console.log(toCurrency);
+  const exchange = async () => {
+    const response = await axios.get(
+      `${BASE_URL}?apikey=${API_KEY}&base_currency=${fromCurrency}`
+    );
+    setResult((response.data.data[toCurrency] * amount).toFixed(2));
+  };
+
+  const change = () => {
+    let temp = fromCurrency;
+    setFromCurrency(toCurrency);
+    setToCurrency(temp);
   };
 
   return (
@@ -23,8 +36,8 @@ const Currency = () => {
             style={{
               marginTop: "-20px",
               fontFamily: "cursive",
-              color: "#fff",
-              background: "black",
+              fontWeight: "bold",
+              padding: "10px",
             }}
           >
             Crental Currency
@@ -39,26 +52,29 @@ const Currency = () => {
             className="value"
           />
           <select
+            value={fromCurrency}
             onChange={(e) => setFromCurrency(e.target.value)}
             className="from-currency-option opt"
           >
-            <option>ZLT</option>
+            <option>PLN</option>
+            <option>TRY</option>
+            <option>USD</option>
+            <option>EUR</option>
           </select>
 
-          <FaLongArrowAltRight
-            style={{
-              fontSize: "25px",
-              color: "yellow",
-              marginRight: "10px",
-              marginTop: "10px",
-            }}
-          />
+          <button onClick={change} className="changeBtn">
+            <FaExchangeAlt />
+          </button>
 
           <select
+            value={toCurrency}
             onChange={(e) => setToCurrency(e.target.value)}
             className="to-currency-option opt"
           >
-            <option>TL</option>
+            <option>TRY</option>
+            <option>PLN</option>
+            <option>USD</option>
+            <option>EUR</option>
           </select>
 
           <input
